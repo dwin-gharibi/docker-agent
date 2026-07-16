8ccf8e80823009cdc05de956f5646e0036ed7ee3
piyush0049 <piyushjoshi81204@gmail.com>
2026-07-16 16:45:57 +0530

    fix(teamloader): preserve deferred toolset instructions via WithNoToolsFilter

--- pkg/teamloader/teamloader.go
+++ pkg/teamloader/teamloader.go
@@ -759,9 +759,7 @@ func getToolsForAgent(ctx context.Context, a *latest.AgentConfig, parentDir stri
 		if !toolset.Defer.IsEmpty() {
 			deferredToolset.AddSource(wrapped, toolset.Defer.DeferAll, toolset.Defer.Tools)
 			if toolset.Defer.DeferAll {
-				// Don't add the wrapped toolset to toolSets - all its tools are deferred
-				// TODO: maybe we _do_ want to add this toolset since it has instructions?
-				continue
+				wrapped = WithNoToolsFilter(wrapped)
 			} else {
 				wrapped = WithToolsExcludeFilter(wrapped, toolset.Defer.Tools...)
 			}
